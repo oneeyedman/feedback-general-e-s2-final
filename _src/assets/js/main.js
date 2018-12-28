1617c93c8cd5c5ee01060be2038e88587422f458
Carlos Mañas <carlos@spacenomads.com>
2018-12-28 10:56:09 +0100

    Add string sample

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -7,11 +7,31 @@ const results = document.querySelector('.results__items');
 function searchTVShows() {
   const api = 'https://api.tvmaze.com/search/shows?q=';
   const query = searchField.value;
-  console.log('yay');
+
   fetch(api+query)
     .then(res => res.json())
     .then(data => {
       // En este punto ya tenemos data
+      let shows = '';
+
+      for (let i=0;i<data.length;i++) {
+
+        let genres = '';
+        for (let x=0;x<data[i].show.genres.length;x++) {
+          genres += `
+            <li class="genre">
+              ${data[i].show.genres[x]}
+            </li>`;
+        }
+
+        shows += `
+          <li class="results__item">
+            <h2 class="result__title">${data[i].show.name}</h2>
+            <ul class="result__genres">${genres}</ul>
+          </li>`;
+      }
+
+      results.innerHTML = shows;
     });
 }
 
